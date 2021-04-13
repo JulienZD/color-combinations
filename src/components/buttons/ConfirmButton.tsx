a524af3998ff2c85a5095e860b660cfcee99e5a7
Julien <jzapata@live.nl>
2021-04-13 19:55:03 +0200

    fix: remove unused button

--- src/components/buttons/ConfirmButton.tsx
+++
@@ -1,5 +0,0 @@
-import { Button, BaseButtonProps } from './Button';
-
-export default function ConfirmButton({ onClick, style, text = 'OK' }: BaseButtonProps): JSX.Element {
-  return <Button onClick={onClick} text={text} icon="check" style={style} />;
-}
--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,4 +1,3 @@
 import { Button, ButtonStyle } from '@components/buttons/Button';
-import ConfirmButton from '@components/buttons/ConfirmButton';
 
-export { Button, ButtonStyle, ConfirmButton };
+export { Button, ButtonStyle };
