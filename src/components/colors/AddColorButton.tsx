740be4be049d53569877df18f1c583326762037d
Julien <jzapata@live.nl>
2021-04-14 12:01:29 +0200

    feat: display color picker to add a new color

--- src/components/colors/AddColorButton.tsx
+++ src/components/colors/AddColorButton.tsx
@@ -1,15 +1,36 @@
+import { ColorPicker } from '@components/colors/ColorPicker';
+import { useCallback, useRef, useState } from 'react';
+import useUnfocus from '@hooks/useUnfocus';
+
 interface Props {
   onSelection: (newColor: string) => void;
 }
 
 export default function AddColorButton({ onSelection }: Props): JSX.Element {
-  const addColor = (): void => onSelection('#fff');
+  const addRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const close = useCallback(() => setIsOpen(false), [isOpen]);
+
+  useUnfocus(addRef, close);
   return (
-    <button
-      className="grid place-content-center p-1 hover:text-secondary transition-colors duration-200"
-      onClick={addColor}
-    >
-      <span className="material-icons text-5xl">add_circle</span>{' '}
-    </button>
+    <div className="relative" ref={addRef}>
+      <button
+        className="grid place-content-center p-1 hover:text-secondary transition-colors duration-200"
+        onClick={(): void => setIsOpen(true)}
+      >
+        <span className="material-icons text-5xl">add_circle</span>{' '}
+      </button>
+      {isOpen && (
+        <ColorPicker
+          color="#FFF"
+          onSelection={(newColor: string): void => {
+            onSelection(newColor);
+            close();
+          }}
+          onCancel={close}
+        />
+      )}
+    </div>
   );
 }
