d9727111235cc66f9f03c8c42967921397ac348c
Julien <jzapata@live.nl>
2021-04-18 12:36:20 +0200

    feat: change individual card colors, add reset button to cards

--- src/components/colors/ColorCard.tsx
+++ src/components/colors/ColorCard.tsx
@@ -1,9 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { ColorCombination } from 'types';
+import PopoverColorPicker from '@components/colors/PopoverColorPicker';
+import calcContrast from '@lib/calcContrast';
 
 interface CardFooterProps {
   colorCombination: ColorCombination;
-  onClick: () => void;
+  swapTheme: () => void;
+  setCardTheme: (newTheme: ColorCombination) => void;
+  resetTheme: () => void;
 }
 
 function OutlinedButton({ color }: { color: string }): JSX.Element {
@@ -18,20 +22,33 @@ function OutlinedButton({ color }: { color: string }): JSX.Element {
   );
 }
 
-function Circle({ color }: { color: string }): JSX.Element {
-  return <div className="inline-block h-4 w-4 rounded-full ml-1" style={{ backgroundColor: color }} />;
+function Circle({ color, onSelection }: { color: string; onSelection: (newColor: string) => void }): JSX.Element {
+  return (
+    <PopoverColorPicker color={color} onSelection={onSelection}>
+      <div className="inline-block h-4 w-4 rounded-full ml-1" style={{ backgroundColor: color }} />
+    </PopoverColorPicker>
+  );
 }
 
-function ColorDescriptor({ text, color }: { text: string; color: string }): JSX.Element {
+function ColorDescriptor({
+  text,
+  color,
+  onSelection,
+}: {
+  text: string;
+  color: string;
+  onSelection: (newColor: string) => void;
+}): JSX.Element {
   return (
     <small className="flex flex-nowrap items-center">
       <span className="whitespace-nowrap">{`${text}: ${color.toLowerCase()}`}</span>
-      <Circle color={color} />
+      <Circle color={color} onSelection={onSelection} />
     </small>
   );
 }
 
-function CardFooter({ colorCombination: { primary, secondary, contrast }, onClick }: CardFooterProps): JSX.Element {
+function CardFooter({ colorCombination, swapTheme, setCardTheme, resetTheme }: CardFooterProps): JSX.Element {
+  const { primary, secondary, contrast } = colorCombination;
   const [copyBtn, setCopyBtnText] = useState({ icon: 'clipboard', title: 'Copy to clipboard' });
   const toClipboard = async (): Promise<void> => {
     if (copyBtn.icon !== 'clipboard') return;
@@ -48,16 +65,39 @@ function CardFooter({ colorCombination: { primary, secondary, contrast }, onClic
   return (
     <div className="p-2 flex justify-between text-gray-300">
       <div className="flex flex-col">
-        <ColorDescriptor text="Primary" color={primary} />
-        <ColorDescriptor text="Secondary" color={secondary} />
+        <ColorDescriptor
+          text="Primary"
+          color={primary}
+          onSelection={(newColor: string): void =>
+            setCardTheme({
+              ...colorCombination,
+              primary: newColor,
+              contrast: calcContrast(newColor, colorCombination.secondary),
+            })
+          }
+        />
+        <ColorDescriptor
+          text="Secondary"
+          color={secondary}
+          onSelection={(newColor: string): void =>
+            setCardTheme({
+              ...colorCombination,
+              secondary: newColor,
+              contrast: calcContrast(colorCombination.primary, newColor),
+            })
+          }
+        />
         <small>Contrast Ratio: {contrast}</small>
       </div>
       <div className="flex justify-end" style={{ color: 'var(--main-text)' }}>
         <button title={copyBtn.title} className="btn-link btn btn-sm" onClick={toClipboard} aria-label={copyBtn.title}>
           <i className={`bi bi-${copyBtn.icon}`} />
         </button>
-        <button className="btn btn-sm btn-link" onClick={onClick} aria-label="Invert colors">
+        <button className="btn btn-sm btn-link" onClick={swapTheme} aria-label="Invert colors">
           <i className="bi bi-circle-half" />
+        </button>
+        <button className="btn btn-sm btn-link" onClick={resetTheme} aria-label="Invert colors">
+          <i className="bi bi-arrow-counterclockwise" />
         </button>
       </div>
     </div>
@@ -65,10 +105,31 @@ function CardFooter({ colorCombination: { primary, secondary, contrast }, onClic
 }
 
 export default function ColorCard(props: ColorCombination): JSX.Element {
-  const [theme, setTheme] = useState({ primary: props.primary, secondary: props.secondary });
-  const { primary, secondary } = theme;
+  const [theme, setTheme] = useState({
+    current: props,
+    previous: props,
+  });
+  const { primary, secondary } = theme.current;
 
-  const swapColors = (): void => setTheme({ primary: secondary, secondary: primary });
+  const swapColors = (): void =>
+    setTheme((prevState) => ({
+      ...prevState,
+      current: { ...prevState.current, primary: secondary, secondary: primary },
+    }));
+
+  const resetTheme = (): void => {
+    setTheme((prevState) => ({
+      current: props,
+      previous: prevState.current,
+    }));
+  };
+
+  const updateTheme = (newTheme: ColorCombination): void => {
+    setTheme((prevState) => ({
+      current: newTheme,
+      previous: prevState.current,
+    }));
+  };
 
   return (
     <article className="m-1">
@@ -90,7 +151,12 @@ export default function ColorCard(props: ColorCombination): JSX.Element {
           </div>
         </div>
       </div>
-      <CardFooter colorCombination={props} onClick={swapColors} />
+      <CardFooter
+        colorCombination={theme.current}
+        swapTheme={swapColors}
+        resetTheme={resetTheme}
+        setCardTheme={updateTheme}
+      />
     </article>
   );
 }
