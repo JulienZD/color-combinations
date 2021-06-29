487966a25e5fcf8017cce4222d343f8fe0a162cb
Julien <jzapata@live.nl>
2021-06-29 15:35:08 +0200

    refactor: extract theme context from `_app.tsx` and optimize it

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,10 +1,10 @@
-import { useCallback, useContext } from 'react';
-import { ThemeContext } from '../pages/_app';
+import { useCallback } from 'react';
+import { useTheme } from '../contexts/theme';
 
-export default function ThemeSwitcher(): JSX.Element {
-  const { theme, swapTheme } = useContext(ThemeContext);
+export default function ThemeSwitcher(): JSX.Element | null {
+  const { theme, swapTheme } = useTheme();
   const swap = useCallback(() => {
     if (swapTheme) swapTheme();
   }, [swapTheme]);
-  return <button onClick={swap}>{theme === 'dark' ? 'Light' : 'Dark'} mode</button>;
+  return theme ? <button onClick={swap}>{theme === 'dark' ? 'Light' : 'Dark'} mode</button> : null;
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,65 +1,11 @@
 import { AppProps } from 'next/app';
 import '../styles/global.css';
-import { createContext, useEffect, useState } from 'react';
-
-interface ThemeContext {
-  theme: 'light' | 'dark';
-  swapTheme: (() => void) | undefined;
-}
-
-export const ThemeContext = createContext<ThemeContext>({
-  theme: 'light',
-  swapTheme: undefined,
-});
+import ThemeProvider from '../contexts/theme';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const schema = '(prefers-color-scheme: dark)';
-
-  function swapTheme(): void {
-    setThemeContext(() => {
-      const newTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
-      if (
-        localStorage.getItem('theme') === 'dark' ||
-        (!('theme' in localStorage) && window.matchMedia(schema).matches)
-      ) {
-        document.documentElement.removeAttribute('class');
-      } else {
-        document.documentElement.classList.add('dark');
-      }
-      localStorage.setItem('theme', newTheme);
-
-      return {
-        theme: newTheme,
-        swapTheme,
-      };
-    });
-  }
-
-  const [themeContext, setThemeContext] = useState<ThemeContext>({
-    theme: 'light',
-    swapTheme,
-  });
-
-  useEffect(() => {
-    const initialTheme =
-      localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia(schema).matches)
-        ? 'dark'
-        : 'light';
-    if (initialTheme === 'dark') {
-      document.documentElement.classList.add('dark');
-    }
-    localStorage.setItem('theme', initialTheme);
-
-    setThemeContext({
-      theme: initialTheme,
-      swapTheme,
-    });
-    // eslint-disable-next-line
-  }, []);
-
   return (
-    <ThemeContext.Provider value={themeContext}>
+    <ThemeProvider>
       <Component {...pageProps} />
-    </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
