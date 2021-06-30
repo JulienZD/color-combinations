6fa0b9fdb17c262e9087b6aad14d16f7c2bb6b72
Julien <jzapata@live.nl>
2021-06-30 21:40:01 +0200

    feat: improve color picker style
    Add a shadow, support light and dark themes

--- src/components/colors/ColorPicker.tsx
+++ src/components/colors/ColorPicker.tsx
@@ -1,6 +1,5 @@
 import { ForwardedRef, forwardRef, useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
-import { Button, ButtonVariant } from '@components/buttons';
 
 interface Props {
   color: string;
@@ -16,26 +15,27 @@ export const ColorPicker = forwardRef<Ref, Props>(({ onSelection, color, onCance
 
   return (
     <>
-      <div ref={ref} className="wrapper p-2 absolute bg-gray-400 rounded-lg flex flex-col">
+      <div
+        ref={ref}
+        className="absolute flex flex-col z-[60] p-4 pb-2 shadow-sharp rounded-r-lg rounded-b-lg border border-transparent dark:border-dark-primary-400 bg-primary-100 dark:bg-dark-primary-600"
+      >
         <div className="flex">
           <HexColorPicker color={currentColor} onChange={(newColor: string): void => setCurrentColor(newColor)} />
           <div className="flex-grow w-5 ml-2 rounded" style={{ backgroundColor: currentColor }} />
         </div>
         <div className="flex justify-end items-center mt-3">
-          <Button variant={ButtonVariant.SECONDARY} className="mr-2" onClick={(): void => onCancel()} label="Cancel" />
-          <Button
-            variant={ButtonVariant.PRIMARY}
+          <button className="btn btn-sm link-themed" onClick={(): void => onCancel()}>
+            Cancel
+          </button>
+          <button
+            className="btn btn-sm btn-primary flex items-center justify-center"
             onClick={(): void => onSelection(currentColor)}
-            label="Select"
-            icon="check"
-          />
+          >
+            <span className="material-icons text-base mr-2">check</span>
+            Select
+          </button>
         </div>
       </div>
-      <style jsx>{`
-        .wrapper {
-          z-index: 60;
-        }
-      `}</style>
     </>
   );
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,9 @@ module.exports = {
           special: '#EDAE49',
         },
       },
+      boxShadow: {
+        sharp: '-2px 6px 7px -2px rgba(0,0,0,0.59)',
+      },
     },
   },
   variants: {
